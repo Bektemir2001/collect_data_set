14c4617e9a5bf06c42c9406c5be11a2d18e56cdd
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2023-10-13 12:02:28 +0600

    save changes

--- resources/views/includes/question_generate.blade.php
+++ resources/views/includes/question_generate.blade.php
@@ -28,7 +28,7 @@
 
 <script>
     let count_of_questions = {{count($context->questions)}};
-    let un_saved_questions = [];
+    let questions = {};
     let current_context_array = splitTextIntoChunks(`{{$context_text}}`, 2500);
     console.log(current_context_array);
     function updateQuestion(question_id)
@@ -84,7 +84,7 @@
                 </div>`;
         div.appendChild(newElement);
     }
-    function saveQuestion(question_id)
+    async function saveQuestion(question_id)
     {
         let question = document.getElementById(`questionNew${question_id}`);
         let answer = document.getElementById(`answerNew${question_id}`);
@@ -94,23 +94,13 @@
             data.append('answer', answer.value);
             data.append('context_id', {{$context->id}});
             let url = "{{route('question.store')}}";
-            fetch(url, {
-                method: "POST",
-                headers: {
-                    'X-CSRF-TOKEN': "{{csrf_token()}}"
-                },
-                body: data
-            })
-                .then(response => response.json())
-                .then(res => {
-                    res = res.data;
-                    let button = document.getElementById(`newButton${question_id}`);
-                    button.innerHTML = 'Update';
-                    button.onclick = `updateQuestion(${res.id})`;
-                    button.className = 'btn btn-primary mr-2';
-                    question.id = `question${res.id}`;
-                    answer.id = `answer${res.id}`;
-                });
+            let res = await fetchQuery(url, data, 'POST');
+            let button = document.getElementById(`newButton${question_id}`);
+            button.innerHTML = 'Update';
+            button.onclick = `updateQuestion(${res.id})`;
+            button.className = 'btn btn-primary mr-2';
+            question.id = `question${res.id}`;
+            answer.id = `answer${res.id}`;
         }
         else{
             alert('empty question or answer');
@@ -119,7 +109,7 @@
     }
 
 
-    function autoGenerate()
+    async function autoGenerate()
     {
         let url = "{{route('question.autogenerate')}}";
         for(let i = 0; i < current_context_array.length; i++)
@@ -128,21 +118,11 @@
             data.append('context_id', {{$context->id}});
             data.append('text', current_context_array[i]);
             if(current_context_array[i].length > 100){
-                fetch(url, {
-                    method:'POST',
-                    headers: {
-                        'X-CSRF-TOKEN': "{{csrf_token()}}"
-                    },
-                    body: data
-                })
-                    .then(response => response.json())
-                    .then(res => {
-                        res = res.data;
-                        for(let i = 0; res.length; i++)
-                        {
-                            addGeneratedQuestion(res[i]);
-                        }
-                    });
+                let res = await fetchQuery(url, data, 'POST');
+                for(let i = 0; res.length; i++)
+                {
+                    addGeneratedQuestion(res[i]);
+                }
             }
         }
     }
@@ -179,5 +159,36 @@
         {
 
         }
+    }
+
+    function fetchQuery(url, form_data, method='GET')
+    {
+        let result;
+        if(method !== 'GET')
+        {
+            fetch(url, {
+                method: method,
+                headers: {
+                    'X-CSRF-TOKEN': "{{csrf_token()}}"
+                },
+                body: form_data
+            })
+                .then(response => response.json())
+                .then(res => {
+                    result = res.data;
+                });
+            return result
+        }
+        fetch(url, {
+            method: method,
+            headers: {
+                'X-CSRF-TOKEN': "{{csrf_token()}}"
+            }
+        })
+            .then(response => response.json())
+            .then(res => {
+                result = res.data;
+            });
+        return result
     }
 </script>
