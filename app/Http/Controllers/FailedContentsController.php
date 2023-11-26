e2755c61378a9cc1d4846e581b0a5ec06fbbfc0a
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2023-11-26 11:29:37 +0600

    save changes

--- app/Http/Controllers/FailedContentsController.php
+++ app/Http/Controllers/FailedContentsController.php
@@ -34,7 +34,6 @@ class FailedContentsController extends Controller
         );
 
         $questions = $this->textService->forGpt4($data['text']);
-        dd($questions);
         $user = auth()->user()->id;
         $this->questionRepository->saveQuestions($questions, $data['context_id'], $user, 'gpt-4');
         return back();
